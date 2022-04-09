69374767f96f0d040408d47ff1c14bf761f0d614
Esra Erdemir <esrardmr99@gmail.com>
2022-04-09 15:57:19 +0300

    Update Esra

--- KutuphaneOturum/Controllers/GirisController.cs
+++ KutuphaneOturum/Controllers/GirisController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KutuphaneOturum.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,65 @@ namespace KutuphaneOturum.Controllers
     public class GirisController : Controller
     {
         // GET: Giris
+        KullaniciEntities K = new KullaniciEntities();
         public ActionResult Giris()
         {
-             return View();
+            Session["SayfaAdi"] = "Giriş";
+            return View();
         }
 
+        public ActionResult Cıkıs()
+        {
+            Session.RemoveAll();
+            return RedirectToAction("Giris");
+        }
+
+        [HttpPost]
+        public ActionResult GirisAction(string usernamea, string password)
+        {
+            if (usernamea.Length > 12 || password.Length > 20)
+            {
+                Session["Uyari"] = "Kullanıcı adı veya şifreniz hatalı. Lütfen tekrar deneyin.";
+            }
+
+            var pss = Hash.md5(password);
+
+            kullanicilar giris = new kullanicilar();
+
+            try
+            {
+                giris = K.kullanicilar.Where(w => w.username == usernamea && w.pass == password).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                giris = null;
+            }
+
+
+
+
+            if (giris != null)
+            {
+
+                {
+
+                }
+                
+                if (giris == null)
+                {
+                    Session["Uyari"] = "Kullanıcı bilgilerine erişilemedi. Lütfen tekrar deneyin. Sorun devam ederse yönetici ile iletişime geçin.";
+
+                    return RedirectToAction("Giris");
+                }  
+            }
+            else
+            {
+                Session["Uyari"] = "Kullanıcı adı veya şifreniz hatalı. Lütfen tekrar deneyin.";
+                return RedirectToAction("Giris");
+            }
+
+            return RedirectToAction("Giris", "Giris");
+        }
+    
     }
 }
---
+++ KutuphaneOturum/Models/Hash.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace KutuphaneOturum.Models
+{
+    public static class Hash
+    {
+        public static string md5(this string password)
+        {
+            if (!string.IsNullOrEmpty(password))
+            {
+                using (var md = MD5.Create())
+                {
+                    var bytes = Encoding.UTF8.GetBytes(password);
+                    var hash = md.ComputeHash(bytes);
+
+                    return Convert.ToBase64String(hash);
+                }
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
